1026e1ba4498db9160ff13d763da313a147c6ece
Phillip Witkin <pwitkin273@gmail.com>
2015-09-02 20:54:28 -0400

    sound.js note block still there

--- public/sound.js
+++ public/sound.js
@@ -85,7 +85,7 @@ var EnvelopeGenerator = (function(context) {
   EnvelopeGenerator.prototype.gateOff = function() {
     console.log("EG gate off")
     now = context.currentTime
-    this.param.setTargetAtTime(0, now + 0.3, this.releaseTime)
+    this.param.setTargetAtTime(0, now + 0.5, this.releaseTime)
     return context.currentTime
   }
 
